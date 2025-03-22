f32aaa71dd9d35e5fa7fa9d5bcdd4622e07db4e1
Seyogi <41766832+seyogi@users.noreply.github.com>
2025-03-22 14:23:31 +0900

    ec2メトリクスのインスタンスの取得

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI, File, UploadFile, Form
+from instance.alb import get_latest_alb_metrics
+from instance.ec2 import get_latest_ec2_metrics
+from instance.rds import get_latest_rds_metrics
 import uvicorn
 
 app = FastAPI()
@@ -13,7 +16,15 @@ async def chat(text: str = Form()):
 
 @app.get("/instances")
 async def get_instances():
-    return {"message": "instances"}
+    metrics_data = {}
+    ec2_metrics = get_latest_ec2_metrics()
+    for metric in ec2_metrics:
+        if metric["cpu_utilization"] is not None:
+            tmp = f"CPU: {metric['cpu_utilization']:.2f}% at {metric['timestamp']}"
+        else:
+            tmp = f"CPUデータが見つかりません"
+        metrics_data[metric["instance_id"]] = tmp
+    return metrics_data
 
 @app.post("/instances/profile")
 async def instance_profile(text: str = Form()):
