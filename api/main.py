27a7359cf1441a520cc64dc11231df2697bc4028
Chroma7p <nahco3.hcl@gmail.com>
2025-03-23 02:31:39 +0900

    arnのif文の修正

--- api/main.py
+++ api/main.py
@@ -19,14 +19,15 @@ app = FastAPI()
 
 
 def get_metrics_by_arn(arn: str):
-    if "alb" in arn:
+    if "elasticloadbalancing" in arn:
         return get_latest_alb_metrics()
     elif "ec2" in arn:
         return get_latest_ec2_metrics()
-    elif "rdb" in arn:
+    elif "rds" in arn:
         return get_latest_rds_metrics()
     else:
         raise Exception(f"サポートされていないARNです: {arn}")
+
 
 
 # ロガーの設定
