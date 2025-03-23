987883dbf7f706d8364da89c368aad661668428c
Seyogi <41766832+seyogi@users.noreply.github.com>
2025-03-23 12:34:48 +0900

    Talkエンドポイントを叩く所まで(CORSが出ます)

--- vue/AWSFriendsOnBrowser/src/App.vue
+++ vue/AWSFriendsOnBrowser/src/App.vue
@@ -6,12 +6,31 @@ export default {
   data(){
     return {
       instances:[],
+      selectedinstance: {
+        type: "",
+        name: ""
+      },
+      chara_txt:"aaa"
     }
   },
+  watch: {
+    selectedinstance() {
+      var log = [
+              {
+                  "role": "user",
+                  "message": ""
+              }
+          ]
+      this.click2(this.selectedinstance.arn,log)
+    },
+  },
   methods: {
-    click: function () {
-      this.instances = tmpWebApi.getInstances()
-    }
+    click: async function () {
+      this.instances = await tmpWebApi.getInstances()
+    },
+    click2: async function (arn,log) {
+      this.chara_txt = await tmpWebApi.postTalk(arn,log)
+    },
   }
 };
 </script>
@@ -22,8 +41,34 @@ export default {
       Header!!!
     </header>
     <input type="button" value="GetInstances" @click="click()" />
+
     <div class="instancelist" v-for="instance in instances" :key="instance">
-      <h3>{{ instance.type }}</h3>
+       <label>
+        <input type="radio" v-model="selectedinstance" :value="instance"/>
+        {{ instance.type }}/{{instance.name}}
+      </label>
+    </div>
+    <br><br>
+    
+    <div v-if="selectedinstance.type != ''">
+      <p>選択されたインスタンス<br>
+      {{ selectedinstance.type }}/{{ selectedinstance.name }}:</p>
+      <textarea
+          v-model="chara_txt"
+          id="instanceTextArea"
+          rows="5"
+          cols="60"
+          readonly
+        />
+      <br>
+
+      あなた:<br>
+      <textarea
+          id="userTextArea"
+          rows="5"
+          cols="60"
+        />
+      <input type="button" value="送信" @click="click2()" />
     </div>
   </div>
 </template>
--- vue/AWSFriendsOnBrowser/src/tmpWebApi.js
+++ vue/AWSFriendsOnBrowser/src/tmpWebApi.js
@@ -1,38 +1,40 @@
 import { axios } from './axios'
 
-var endpoint = "instances"
-var url = 'https://aws-friends.k1h.dev/' + endpoint
+var endpoint = 'https://aws-friends.k1h.dev/'
+var url1 = endpoint + "instances"
+var url2 = endpoint + "talk"
 
 export default {
-    getInstances:function(){
-        console.log({ axios: this.axios });
-        axios.get(url)
-            .then((response) => {
-                console.log("データの取得に成功しました");
-                console.log(response.data);
-                return response.data
-            })
-            .catch((error) => {
-                console.error("データ取得に失敗しました", error);
-            });
+    getInstances:async function(){
+        try {
+            console.log({ axios: this.axios });
+            const response = await axios.get(url1);
+            console.log("データの取得に成功しました");
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            console.error("データ取得に失敗しました", error);
+        }
     },
-    sendemail: function (subject,name,text) {
-        console.log({ axios: this.axios });
+    postTalk:async function (arn,log) {
         const options = {
             headers: {
-                "content-type": "multipart/form-data",
+                "content-type": "application/json",
             }
         }
-        axios.post(url, contents, options)
-            .then(function (res) {
-                console.log(res);
-                if (res.data == "error")
-                    alert("テキストが入力されていません")
-                else
-                    alert("送信しました")
-            }).catch(function (res) {
-                alert("ネットワークエラー");
-                console.log(res);
-            });
+        const contents = {
+            "arn": arn,
+            "log": log
+        }
+        console.log(contents)
+        try {
+            console.log({ axios: this.axios });
+            const response = await axios.post(url2, contents, options);
+            console.log("データの取得に成功しました");
+            return response.data;
+        } catch (error) {
+            alert("ネットワークエラー");
+            console.error(error);
+        }
     }
 }
